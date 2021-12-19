87a987ae212e92c573306409016f409409cb35d7
Alexander <oorzhakau@yandex.ru>
2021-12-19 05:04:14 +0300

    first version

---
+++ landingpagewebsite/crm/views.py
@@ -0,0 +1,39 @@
+from django.shortcuts import render
+from .models import Order
+from .forms import OrderForm
+from cms.models import CmsSlider
+from price.models import PriceTable, PriceCart
+from telebot.sendmessage import sendTelegram
+
+def first_page(request):
+    slider_list = CmsSlider.objects.all()
+    pc_1 = PriceCart.objects.get(pk=1)
+    pc_2 = PriceCart.objects.get(pk=2)
+    pc_3 = PriceCart.objects.get(pk=3)
+    price_table = PriceTable.objects.all()
+    form = OrderForm()
+    dict_obj = {
+                'slider_list': slider_list,
+                'pc_1': pc_1,
+                'pc_2': pc_2,
+                'pc_3': pc_3,
+                'price_table': price_table,
+                'form': form,
+                }
+    return render(request,
+                  './index.html',
+                  dict_obj)
+
+
+def thanks_page(request):
+    if request.POST:
+        name = request.POST['name']
+        phone = request.POST['phone']
+        element = Order(order_name=name, order_phone=phone)
+        element.save()
+        sendTelegram(tg_name=name, tg_phone=phone)
+        return render(request, './thanks.html',
+            {'name': name,
+            'phone': phone})
+    else:
+        return render(request, './thanks.html')
---
+++ landingpagewebsite/price/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import PriceCart, PriceTable
+
+
+admin.site.register(PriceCart)
+admin.site.register(PriceTable)
